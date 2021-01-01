a104b5d4c2d5ff1e57f1178b9ff2633864dd53d0
Matthew Tzou <sonic55099@gmail.com>
2021-01-01 13:30:59 -0500

    added all available black keys

--- CanvasPiano.py
+++ CanvasPiano.py
@@ -49,33 +49,23 @@ a5 = canvas.create_polygon(950, 0, 950, 300, 1000, 300, 1000, 0, fill='gainsboro
 b5 = canvas.create_polygon(1000, 0, 1000, 300, 1050, 300, 1050, 0, fill='gainsboro', outline='light steel blue')
 c6 = canvas.create_polygon(1050, 0, 1050, 300, 1100, 300, 1100, 0, fill='gainsboro', outline='light steel blue')
 
-
-"""
-c4 = canvas.create_polygon(0, 0, 0, 300, 50, 300, 50, 0, fill='gainsboro', outline='light steel blue')
-d4 = canvas.create_polygon(50, 0, 50, 300, 100, 300, 100, 0, fill='gainsboro', outline='light steel blue')
-e4 = canvas.create_polygon(100, 0, 100, 300, 150, 300, 150, 0, fill='gainsboro', outline='light steel blue')
-f4 = canvas.create_polygon(150, 0, 150, 300, 200, 300, 200, 0, fill='gainsboro', outline='light steel blue')
-g4 = canvas.create_polygon(200, 0, 200, 300, 250, 300, 250, 0, fill='gainsboro', outline='light steel blue')
-"""
-
-as3 = canvas.create_polygon(35, 0, 35, 200, 65, 200, 65, 0, fill='black')
-cs4 = canvas.create_polygon(135, 0, 135, 200, 165, 200, 165, 0, fill='black')
-ds4 = canvas.create_polygon(185, 0, 185, 200, 215, 200, 215, 0, fill='black')
-fs4 = canvas.create_polygon(285, 0, 285, 200, 315, 200, 315, 0, fill='black')
-gs4 = canvas.create_polygon(335, 0, 335, 200, 365, 200, 365, 0, fill='black')
-as4 = canvas.create_polygon(385, 0, 385, 200, 415, 200, 415, 0, fill='black')
-cs5 = canvas.create_polygon(485, 0, 485, 200, 515, 200, 515, 0, fill='black')
-ds5 = canvas.create_polygon(535, 0, 535, 200, 565, 200, 565, 0, fill='black')
-fs5 = canvas.create_polygon(635, 0, 635, 200, 665, 200, 665, 0, fill='black')
-gs5 = canvas.create_polygon(685, 0, 685, 200, 715, 200, 715, 0, fill='black')
-as5 = canvas.create_polygon(735, 0, 735, 200, 765, 200, 765, 0, fill='black')
+cs3 = canvas.create_polygon(35, 0, 35, 200, 65, 200, 65, 0, fill='black')
+ds3 = canvas.create_polygon(85, 0, 85, 200, 115, 200, 115, 0, fill='black')
+fs3 = canvas.create_polygon(185, 0, 185, 200, 215, 200, 215, 0, fill='black')
+gs3 = canvas.create_polygon(235, 0, 235, 200, 265, 200, 265, 0, fill='black')
+as4 = canvas.create_polygon(285, 0, 285, 200, 315, 200, 315, 0, fill='black')
+cs4 = canvas.create_polygon(385, 0, 385, 200, 415, 200, 415, 0, fill='black')
+ds4 = canvas.create_polygon(435, 0, 435, 200, 465, 200, 465, 0, fill='black')
+fs4 = canvas.create_polygon(535, 0, 535, 200, 565, 200, 565, 0, fill='black')
+gs4 = canvas.create_polygon(585, 0, 585, 200, 615, 200, 615, 0, fill='black')
+as5 = canvas.create_polygon(635, 0, 635, 200, 665, 200, 665, 0, fill='black')
+cs5 = canvas.create_polygon(735, 0, 735, 200, 765, 200, 765, 0, fill='black')
+ds5 = canvas.create_polygon(785, 0, 785, 200, 815, 200, 815, 0, fill='black')
+fs5 = canvas.create_polygon(885, 0, 885, 200, 915, 200, 915, 0, fill='black')
+gs5 = canvas.create_polygon(935, 0, 935, 200, 965, 200, 965, 0, fill='black')
 
 
-"""
-cs4 = canvas.create_polygon(35, 0, 35, 200, 65, 200, 65, 0, fill='black')
-ds4 = canvas.create_polygon(85, 0, 85, 200, 115, 200, 115, 0, fill='black')
-fs4 = canvas.create_polygon(185, 0, 185, 200, 215, 200, 215, 0, fill='black')
-"""
+
 
 # canvas.tag_bind(f4, "<Return>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/f4.mp3'))
 
@@ -102,15 +92,22 @@ canvas.tag_bind(a5, "<Button-1>", lambda event: buttonClick(event, path='resourc
 canvas.tag_bind(b5, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/b5.mp3'))
 canvas.tag_bind(c6, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/c6.mp3'))
 
-
-
+canvas.tag_bind(cs3, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/c-3.mp3'))
+canvas.tag_bind(ds3, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/d-3.mp3'))
+canvas.tag_bind(fs3, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/f-3.mp3'))
+canvas.tag_bind(gs3, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/g-3.mp3'))
 canvas.tag_bind(as4, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/a-4.mp3'))
-
-"""
 canvas.tag_bind(cs4, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/c-4.mp3'))
 canvas.tag_bind(ds4, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/d-4.mp3'))
 canvas.tag_bind(fs4, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/f-4.mp3'))
-"""
+canvas.tag_bind(gs4, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/g-4.mp3'))
+canvas.tag_bind(as5, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/a-5.mp3'))
+canvas.tag_bind(cs5, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/c-5.mp3'))
+canvas.tag_bind(ds5, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/d-5.mp3'))
+canvas.tag_bind(fs5, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/f-5.mp3'))
+canvas.tag_bind(gs5, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/g-5.mp3'))
+canvas.tag_bind(as5, "<Button-1>", lambda event: buttonClick(event, path='resources/sounds/mp3Notes/a-5.mp3'))
+
 
 
 root.mainloop()
